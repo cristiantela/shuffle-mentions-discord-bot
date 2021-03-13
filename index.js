b77d376a983b49f7fe50bc5e22307c24e9187d3d
Matheus Cristian <mathues@email.com>
2021-03-13 12:26:47 -0300

    Adds poll funcionality

--- index.js
+++ index.js
@@ -8,9 +8,172 @@ client.once("ready", () => {
   console.log("Bot Shuffle is online!");
 });
 
+const readPoll = (raw) => {
+  const content = raw.split("\n");
+
+  const header = content[1].match(/^\/\/ poll ([a-z0-9]+)( \((closed)\))?/);
+
+  const poll = {
+    id: header[1],
+    closed: !!header[3],
+    title: "",
+    options: {},
+  };
+
+  poll.title = content[2];
+
+  content.slice(3).forEach((option, index) => {
+    const name = option.match(/^(..) (\d+% \((\d+)\) )?(.+)$/);
+
+    const emoji = name[1];
+    let total = name[3] ? Number(name[3]) : 0;
+    const title = name[4];
+
+    poll.options[emoji] = {
+      text: title,
+      total: total,
+    };
+  });
+
+  return poll;
+};
+
+const calculatePercentage = (poll, votes) => {
+  const totalVotes = Object.keys(poll.options).reduce(
+    (previous, emoji) => previous + poll.options[emoji].total,
+    0
+  );
+
+  return Math.floor((votes / totalVotes) * 1e2);
+};
+
+const renderPollMessage = (poll) => {
+  const message = ["```js", `// poll ${poll.id}`];
+
+  if (poll.closed) {
+    message[1] = message[1] + " (closed)";
+  }
+
+  message.push(poll.title);
+
+  Object.keys(poll.options).forEach((emoji) => {
+    poll.options[emoji];
+
+    const percentage = calculatePercentage(poll, poll.options[emoji].total);
+
+    if (percentage) {
+      message.push(
+        `${emoji} ${percentage}% (${poll.options[emoji].total}) ${poll.options[emoji].text}`
+      );
+    } else {
+      message.push(`${emoji} ${poll.options[emoji].text}`);
+    }
+  });
+
+  return message.join("\n");
+};
+
+client.on("messageReactionAdd", (messageReaction, user) => {
+  if (user.id === client.user.id) {
+    console.log("it was the bot");
+    return false;
+  }
+
+  if (messageReaction.message.author.id !== client.user.id) {
+    console.log("it was not a message from the bot");
+    return false;
+  }
+
+  const poll = readPoll(messageReaction.message.content);
+
+  if (poll.closed) {
+    console.log("poll closed");
+    return false;
+  }
+
+  if (poll.options[messageReaction._emoji.name]) {
+    poll.options[messageReaction._emoji.name].total++;
+  }
+
+  messageReaction.message.edit(renderPollMessage(poll));
+});
+
+client.on("messageReactionRemove", (messageReaction, user) => {
+  console.log(messageReaction, user);
+  if (user.id === client.user.id) {
+    console.log("it was the bot");
+    return false;
+  }
+
+  if (messageReaction.message.author.id !== client.user.id) {
+    console.log("it was not a message from the bot");
+    return false;
+  }
+
+  const content = messageReaction.message.content.split("\n");
+
+  const poll = readPoll(messageReaction.message.content);
+
+  if (poll.closed) {
+    console.log("poll closed");
+    return false;
+  }
+
+  if (poll.options[messageReaction._emoji.name]) {
+    poll.options[messageReaction._emoji.name].total--;
+  }
+
+  messageReaction.message.edit(renderPollMessage(poll));
+});
+
 client.on("message", (message) => {
   if (message.author.id === client.user.id) {
     return false;
+  }
+
+  if (message.content.startsWith("create poll")) {
+    const emojis = ["🟥", "🟧", "🟨", "🟩", "🟦", "🟪", "⬛️", "⬜️", "🟫"];
+
+    const args = message.content.split("\n");
+
+    const id = (
+      Math.floor(Math.random() * (Math.pow(36, 4) - 1 - Math.pow(36, 3))) +
+      Math.pow(36, 3)
+    ).toString(36);
+
+    const response = ["```js", `// poll ${id}`, `\`\`\`**${args[1]}**`];
+
+    const options = args.slice(2);
+
+    options.forEach((option, index) => {
+      response.push(`${emojis[index]} ${option}`);
+    });
+
+    message.channel.send(response.join("\n")).then(function (message) {
+      options.forEach((option, index) => {
+        message.react(emojis[index]);
+      });
+    });
+  }
+
+  if (/^(close|open) poll /.test(message.content)) {
+    message.channel.messages.fetch({ limit: 50 }).then((messages) => {
+      const [, status, id] = message.content.match(
+        /^(close|open) poll ([a-z0-9]+)/
+      );
+
+      const pattern = `\`\`\`js\n// poll ${id}`;
+
+      messages.forEach((message) => {
+        if (message.content.startsWith(pattern)) {
+          const poll = readPoll(message.content);
+
+          poll.closed = status === "close";
+
+          message.edit(renderPollMessage(poll));
+        }
+      });
+    });
   }
 
   if (!message.content.startsWith("shuffle ")) {
